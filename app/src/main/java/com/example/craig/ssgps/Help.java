f55d805107442ee997e3e66648e3e2bf2fd5dcbb
CraigSubnarine <craig.subnarine@gmail.com>
2017-04-08 18:05:36 -0400

    contacts to addContacts linked

--- app/src/main/java/com/example/craig/ssgps/Help.java
+++ app/src/main/java/com/example/craig/ssgps/Help.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -22,8 +24,18 @@ public class Help extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View v =inflater.inflate(R.layout.fragment_help, container, false); 
+        ((Button)v.findViewById(R.id.help_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMS();
+            }
+        });
+        return v;
+    }
+
+    private void sendSMS() {
+        Toast.makeText(getContext(), "Should sednd sms herree", Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/example/craig/ssgps/MainActivity.java
+++ app/src/main/java/com/example/craig/ssgps/MainActivity.java
@@ -15,9 +15,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private String email;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +32,25 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        email = getIntent().getStringExtra("email");
+        name = getIntent().getStringExtra("name");
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                ((TextView)drawerView.findViewById(R.id.email)).setText(email);
+                ((TextView)drawerView.findViewById(R.id.name)).setText(name);
+                super.onDrawerOpened(drawerView);
+            }
+        };
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
 
         Help helpFragment = new Help();
         FragmentManager manager = getSupportFragmentManager();
@@ -88,7 +105,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
             return true;
         }
 
