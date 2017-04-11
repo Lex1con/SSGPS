b76c808b336bbdf363efba0074924b648dddb2be
Lex1con <mikkel.hayes61@gmail.com>
2017-04-11 00:37:56 -0400

    Geofence monitoring in Location service

--- app/src/main/java/com/example/craig/ssgps/FireBaseActivity.java
+++ app/src/main/java/com/example/craig/ssgps/FireBaseActivity.java
@@ -119,5 +119,4 @@ public class FireBaseActivity extends AppCompatActivity {
         Log.d("FireBaseActivity","Contact Added");
 
     }
-
 }
--- app/src/main/java/com/example/craig/ssgps/LocationService.java
+++ app/src/main/java/com/example/craig/ssgps/LocationService.java
@@ -2,6 +2,7 @@ package com.example.craig.ssgps;
 
 import android.*;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,20 +12,30 @@ import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Mikkel on 09/04/2017.
  */
 
-public class LocationService extends IntentService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, CloseListener{
+public class LocationService extends IntentService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, CloseListener, ResultCallback<Status> {
 
     private static final String NAME = "LocationService";
     Location lastlocation;
@@ -32,14 +43,18 @@ public class LocationService extends IntentService implements GoogleApiClient.Co
     LocationRequest mLocationRequest = LocationRequest.create();
     protected com.google.android.gms.location.LocationListener glistener;
 
+    public List uwiFenceList;
+    public List FenceDetails;
+
     protected ConnectivityManager connManager;
     protected NetworkInfo mWifi;
-
 
     protected LocationListener listener;
     protected LocationManager manager;
 
     private boolean hasPermission = false;
+
+
 
 //    SQLiteOpenHelper helper = new DBHelper(this);
 //    final SQLiteDatabase db = helper.getWritableDatabase();
@@ -78,10 +93,6 @@ public class LocationService extends IntentService implements GoogleApiClient.Co
             };
             mGoogleApiClient.connect();
 //            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest, glistener);
-
-
-
-
         }
         else{
             Log.d("LocationService","No internet connection, Using Device GPS");
@@ -114,7 +125,7 @@ public class LocationService extends IntentService implements GoogleApiClient.Co
                 Log.d(NAME, "Unable to access GPS");
             } else {
                 hasPermission = true;
-                manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 5, listener);
+                manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 5, listener);
 
             }
         }
@@ -137,7 +148,7 @@ public class LocationService extends IntentService implements GoogleApiClient.Co
     public void onConnected(Bundle bundle) {
         Log.d("Location Service", "Google Client connected");
         lastlocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        mLocationRequest.setInterval(500);
+        mLocationRequest.setInterval(60000);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d("Location service","No permission Granted");
 
@@ -147,6 +158,7 @@ public class LocationService extends IntentService implements GoogleApiClient.Co
             }
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, glistener);
         }
+        addFences();
     }
 
     @Override
@@ -167,7 +179,119 @@ public class LocationService extends IntentService implements GoogleApiClient.Co
 //
 //    }
 
+    public void addFences(){
+        uwiFenceList = new ArrayList();
+        FenceDetails = new ArrayList();
+        float radius = 150;
+        Map details = new HashMap();
+        details.put("latitude",10.643121452729275 );
+        details.put("longitude",-61.40167124569416 );
+        details.put("radius",150 );
+        details.put("ID", 0);
+
+        uwiFenceList.add(new Geofence.Builder()
+                .setRequestId(String.valueOf(0))
+                .setCircularRegion(
+                        (double)details.get("latitude"),
+                        (double)details.get("longitude"),
+                        radius
+                )
+                .setExpirationDuration(-1)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build()
+        );
+
+        FenceDetails.add(details);
+
+        details = new HashMap();
+        details.put("latitude",10.643922815731743 );
+        details.put("longitude",-61.398724503815174 );
+        details.put("radius",150 );
+        details.put("ID", 1);
+
+        uwiFenceList.add(new Geofence.Builder()
+                .setRequestId(String.valueOf(1))
+                .setCircularRegion(
+                        (double)details.get("latitude"),
+                        (double)details.get("longitude"),
+                        radius
+                )
+                .setExpirationDuration(-1)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build()
+        );
+
+        FenceDetails.add(details);
+
+        details = new HashMap();
+        details.put("latitude",10.641828130118515 );
+        details.put("longitude",-61.39747995883227 );
+        details.put("radius",150 );
+        details.put("ID", 2);
+
+        uwiFenceList.add(new Geofence.Builder()
+                .setRequestId(String.valueOf(2))
+                .setCircularRegion(
+                        (double)details.get("latitude"),
+                        (double)details.get("longitude"),
+                        radius
+                )
+                .setExpirationDuration(-1)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                .build()
+        );
+
+        FenceDetails.add(details);
+        handleGeoFence();
+    }
+
+    public void handleGeoFence(){
+
+        /*
+        handleGeoFence takes the list of fences and places them on the map and calls the GeoFence Services.
+        */
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("LocationService", "Attempting to setup Add a GeoFence");
+            if (uwiFenceList != null &&  uwiFenceList.size() > 0) {
+
+                LocationServices.GeofencingApi.addGeofences(
+                        mGoogleApiClient,
+                        getGeofencingRequest(),
+                        getGeofencePendingIntent()
+                ).setResultCallback(this);
+            }else{
+                Log.d("LocationService", "No values in the geoFence List");
+                return;
+            }
+        }else{
+            Log.d("LocationService", "Permission for Location not given");
+            return;
+        }
+        Log.d("LocationService", "Geofence setup complete");
+
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(uwiFenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        return PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
+    }
 
 
+    @Override
+    public void onResult(@NonNull Status status) {
+
+    }
 }
 
